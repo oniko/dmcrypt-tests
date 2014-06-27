356a97b1ae71ed23e47f0c001bdac39c92f3c91e
Ondrej Kozina <okozina@redhat.com>
2014-06-27 13:16:53 +0200

    experimental script disabling cpu throttling

---
+++ disable-power-saving.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Simple script trying to achieve more stable results by disabling
+# cpu throtling. This is first draft, there is no way to restore original
+# values.
+#
+
+. ./t-dm-lib.sh
+
+function disable_cpu_throttling()
+{
+	pdebug "Disabling cpu throttling"
+
+	test -f /sys/devices/system/cpu/intel_pstate/no_turbo && echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+	
+	test -f /sys/devices/system/cpu/cpufreq/boost && echo 0 > /sys/devices/system/cpu/cpufreq/boost
+
+	test -d /sys/devices/system/cpu/cpu0/cpuidle || exit 0
+
+	for cpu in /sys/devices/system/cpu/cpu* ; do
+		test ${cpu: -7} != "cpuidle" || continue
+		for cstate in $cpu/cpuidle/state* ; do
+			test ${cstate##*/state} -ne 0 || continue
+			echo 1 > $cstate/disable
+		done
+	done
+}
+
+function enable_cpu_throttling()
+{
+	pdebug "Enabling cpu throttling"
+
+	test -f /sys/devices/system/cpu/intel_pstate/no_turbo && echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
+	test -f /sys/devices/system/cpu/cpufreq/boost && echo 1 > /sys/devices/system/cpu/cpufreq/boost
+
+	test -d /sys/devices/system/cpu/cpu0/cpuidle || exit 0
+
+	for cpu in /sys/devices/system/cpu/cpu* ; do
+		test ${cpu: -7} != "cpuidle" || continue
+		for cstate in $cpu/cpuidle/state* ; do
+			test ${cstate##*/state} -ne 0 || continue
+			echo 0 > $cstate/disable
+		done
+	done
+}
+
+disable_cpu_throttling
+enable_cpu_throttling
